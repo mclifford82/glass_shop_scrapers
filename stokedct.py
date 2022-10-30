daf8e2b46e378dc2569abbf3c6fcee4c950f1d20
Martin Clifford <mclifford82@gmail.com>
2022-10-30 01:19:05 -0700

    Moved write_product_to_file to util/util.py and completed the stokedct.py scraper.

--- stokedct.py
+++ stokedct.py
@@ -1,60 +1,70 @@
 import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-from util.artists import artists
+from datetime import date
+from util.util import write_product_to_file
 
-# base_url = 'https://stokedct.com/collections/'
-# url_list = [
-#   base_url + artist.lower().replace(' ', '-') 
-#   for artist in artists
-#]
+# Cycle through pages, collecting info from each item and then outputting to a csv file
+count = 0
+base_url = 'https://stokedct.com/collections/all'
+
+# access first page to get the total item count, divided by 36 rounded up to get the range(1, X, 1) value
+p = urlopen(base_url)
+h = p.read().decode('utf-8')
+s = BeautifulSoup(h, 'html.parser')
+
+product_count_text = s.select('div.number-of-products.pull-right')[0].text
+product_count = int(re.search(r'(\d+)\s.+', product_count_text, re.I).group(1))
+page_count = round(product_count / 36) + 1
 
 url_list = [
-  'https://stokedct.com/collections/steve-h',
-  'https://stokedct.com/collections/babedrienne',
+  f'https://stokedct.com/collections/all?page={page}'
+  for page in range(1, page_count, 1)
 ]
 
-product_list = []
-
-for url in url_list:
+for page_num, url in enumerate(url_list):
   artist_page = urlopen(url)
   artist_html = artist_page.read().decode('utf-8')
   artist_soup = BeautifulSoup(artist_html, 'html.parser')
 
-  product_count_text = artist_soup.select('div.number-of-products.pull-right')[0].text
-  product_count = int(re.search(r'(\d+)\s.+', product_count_text, re.I).group(1))
-
-  print(f'{url} - {product_count} product(s) found.')
-
-  if product_count == 0:
-    continue
-
   products = artist_soup.find_all("div", {"class": "product_inside"})
-  
+
   for product in products:
     p_fulltitle = product.find('img')['alt']
     
-    # Extract item name after the hyphen (is this consistent for all artists)
-    p_item_name = re.search(r'-\s(.+)', p_fulltitle).group(1)
+    # Extract item name after the hyphen if one is present. If not, take the whole name
+    if '-' in p_fulltitle:
+      p_item_artist = re.search(r'(.+)\s?-\s?(.+)', p_fulltitle).group(1).strip()
+      p_item_name = re.search(r'(.+)\s?-\s?(.+)', p_fulltitle).group(2).strip()
+    else:
+      p_item_artist = 'Stoked CT Store'
+      p_item_name = p_fulltitle
     
     p_item_img_link = product.find('img')['src']
 
     p_item_link = 'https://stokedct.com' + product.find('a')['href']
 
-    p_item_desc = product.find('div', {'class':'description'}).text
-    p_item_desc = p_item_desc.replace(u"\u00A0", " ") #remove non breaking spaces
+    #p_item_desc = product.find('div', {'class':'description'}).text
+    #p_item_desc = p_item_desc.replace(u"\u00A0", " ") #remove non breaking spaces
     
     p_item_price = product.find('div', {'class':'price'}).span.text
+    p_item_price = re.sub(r'[$,]', '', p_item_price, re.I) # 
 
-    p_item_old_price = product.select('span.old-price.hide')[0].text
+    #p_item_old_price = product.select('span.old-price.hide')[0].text
     
     item_dict = {
+      'artist': p_item_artist,
       'name': p_item_name,
       'img': p_item_img_link,
       'link': p_item_link,
-      'desc': p_item_desc,
+      #'desc': p_item_desc,
       'price': p_item_price,
-      'old_price': p_item_old_price,
+      #'old_price': p_item_old_price,
     }
 
-    #print(item_dict)
+    write_product_to_file(item_dict)
+    count += 1
+
+  print(f'Page {page_num+1} - Current count is {count}')
+  
+print(f'We fuckin did it! {count} items downloaded!')
---
+++ util/util.py
@@ -0,0 +1,21 @@
+from datetime import date
+import os.path
+import csv
+
+def write_product_to_file(product):
+  """Convert dictionary to csv lines"""
+  datestr = date.today().strftime('%Y%m%d')
+  filename = f'stokedct_{datestr}.txt'
+  if not os.path.exists(filename):
+    with open(filename, 'w', newline='\n') as newfile:
+      newfile.write('artist,name,img,link,price\n')
+
+  with open(filename, 'a+', newline='\n') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow([
+      product['artist'],
+      product['name'],
+      product['img'],
+      product['link'],
+      product['price'],
+    ])
